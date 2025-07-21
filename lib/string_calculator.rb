e1571e6a4eb807097036027b56790844c38309b8
Namitha chandran <namithachandran003@gmail.com>
2025-07-21 20:44:41 +0530

    Added different delimiters.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,15 @@
 class StringCalculator
   def add(str_numbers)
     return str_numbers.to_i if str_numbers.length == 1
-    numbers = str_numbers.split(/,|\n/).map(&:to_i)
+
+    delimiter = ',|\n'
+    if str_numbers.start_with?('//')
+      delimiter = str_numbers[2]
+      str_numbers = str_numbers[4..]
+    end
+
+    numbers = str_numbers.split(Regexp.new(delimiter)).map(&:to_i)
+
     numbers.sum
   end
 end
